38333ad68669272a09c3155351f6358a988c4024
grazionale <gabrielgrazionale@hotmail.com>
2018-04-04 19:44:16 -0300

    04-04-2018

--- src/view/Home.java
+++ src/view/Home.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import controler.ClienteController;
@@ -10,15 +5,9 @@ import controler.PaisController;
 import model.Cliente;
 import model.Pais;
 
-/**
- *
- * @author gabri
- */
 public class Home extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Home
-     */
+
     public Home() {
         initComponents();
     }
